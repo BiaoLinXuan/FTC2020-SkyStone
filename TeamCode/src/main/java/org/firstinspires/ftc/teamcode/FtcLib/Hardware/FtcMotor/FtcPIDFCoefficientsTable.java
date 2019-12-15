0424eaee8ae2a0d4404d1c9027b6a60db09a89ef
blx <1423296426@qq.com>
2019-12-15 22:17:49 +0800

    重新整定PIDF参数

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcLib/Hardware/FtcMotor/FtcPIDFCoefficientsTable.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcLib/Hardware/FtcMotor/FtcPIDFCoefficientsTable.java
@@ -1,6 +1,8 @@
 package org.firstinspires.ftc.teamcode.FtcLib.Hardware.FtcMotor;
 
 public class FtcPIDFCoefficientsTable {
-    public static FtcPIDFCoefficients CHASSIS_PIDF = new FtcPIDFCoefficients
-            (2,0,0,0,1);
+    public static FtcPIDFCoefficients CHASSIS_POSITION = new FtcPIDFCoefficients
+            (4,3,5,10,5);
+    public static FtcPIDFCoefficients CHASSIS_VELOCITY = new FtcPIDFCoefficients
+            (4,3,2,10);
 }
