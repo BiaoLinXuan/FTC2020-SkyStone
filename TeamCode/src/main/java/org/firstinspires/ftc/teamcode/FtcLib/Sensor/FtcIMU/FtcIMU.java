5f9dfc28ba9e89111c408d91a20604987208cba5
blx <1423296426@qq.com>
2019-12-22 23:47:32 +0800

    更新IMU

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcLib/Sensor/FtcIMU/FtcIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FtcLib/Sensor/FtcIMU/FtcIMU.java
@@ -6,31 +6,53 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Position;
+import org.firstinspires.ftc.robotcore.external.navigation.Velocity;
+import org.firstinspires.ftc.teamcode.FtcLib.Algorithm.ConvertAxes2D.Vector2D;
 
 public class FtcIMU {
-    BNO055IMU imu;
-    double reference = 0;
+    public BNO055IMU imu;
+    public float reference = 0;
 
     public FtcIMU(String name, HardwareMap hardwareMap) {
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
-        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+
         parameters.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        parameters.calibrationDataFile = "BNO055IMUCalibration.json";
+        parameters.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        parameters.calibrationDataFile = "AdafruitIMUCalibration.json";
         parameters.loggingEnabled = false;
+        parameters.mode = BNO055IMU.SensorMode.IMU;
+        parameters.useExternalCrystal = true;
+
         imu = hardwareMap.get(BNO055IMU.class, "imu");
         imu.initialize(parameters);
+        imu.startAccelerationIntegration(new Position(),new Velocity(),10);
     }
 
     public void resetReference() {
         reference = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
     }
 
-    public double getYaw() {
-        double rotation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - reference;;
+    public float getYaw() {
+        float rotation = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle - reference;;
         if (Math.abs(rotation) > 180) {
             if (reference >= 0) rotation += 360;
             if (reference < 0) rotation -= 360;
         }
         return rotation;
+    }
+
+    public float getRawYaw() {
+        return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES).firstAngle;
+    }
+
+    public Vector2D getSpeedVector(){
+        Velocity velocity = imu.getVelocity();
+        return new Vector2D(velocity.xVeloc,velocity.yVeloc);
+    }
+
+    public Vector2D getPositionVector(){
+        Position position = imu.getPosition();
+        return new Vector2D(position.x,position.y);
     }
 }
